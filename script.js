e4d81abb324f2d2fa43d599e3e164a7f079edd9b
karinabentancor <karinabentacorlenzi@gmail.com>
2024-10-16 23:48:00 -0300

    text-carousel

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+    const texts = [
+        "Texto aleatorio 1",
+        "Texto aleatorio 2",
+        "Texto aleatorio 3",
+        "Texto aleatorio 4",
+        "Texto aleatorio 5",
+    ];
+
+    const carouselItems = document.querySelectorAll('.carousel-item h5');
+
+    carouselItems.forEach((item, index) => {
+        item.textContent = texts[Math.floor(Math.random() * texts.length)];
+    });
+
 
 
